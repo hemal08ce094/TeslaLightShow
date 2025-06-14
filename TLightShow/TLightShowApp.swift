a409fd34bf840b7a47d0661b3ea265cb271ece1a
hemal <hemal08ce094@gmail.com>
2025-06-14 10:10:54 -0700

    Initial Commit

---
+++ TLightShow/TLightShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TLightShowApp.swift
+//  TLightShow
+//
+//  Created by hemal on 14/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct TLightShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
